89c12c0724125e2a8ab99a8599f28cde061fa23d
Cratesmith <cratesmith@cratesmith.com>
2019-08-05 23:52:48 +1000

    package setup

---
+++ EventDispatcher.cs
@@ -0,0 +1,234 @@
+using System;
+using System.Collections.Generic;
+using Cratesmith.Utils;
+
+public class EventDispatcher
+{
+    /// <summary>
+    /// The global dispatcher. 
+    /// It's not advised to send events to it however, as most things have local dispatchers
+    /// </summary>
+    public static readonly EventDispatcher Global = new EventDispatcher();
+
+    public void Add(EventDispatcher otherDispatcher)
+    {
+        if(otherDispatcher==null) throw new ArgumentNullException();
+        m_otherDispatchers.Add(otherDispatcher);
+    }
+
+    public void Remove(EventDispatcher otherDispatcher)
+    {
+        m_otherDispatchers.Remove(otherDispatcher);
+    }
+
+	public void Add<T>(Action<T> listener)
+	{
+		var table = Get<T>();
+		table.dispatch += listener;
+	}
+
+	public void Remove<T>(Action<T> listener)
+	{
+		var table = Get<T>();
+		table.dispatch -= listener;
+	}
+
+	public void Clear()
+	{
+		m_dispatchers.Clear();
+	}
+
+    /// <summary>
+	/// Creates a disposable dispatch wrapper. 
+	/// </summary>
+	/// <typeparam name="T">Type of the message payload</typeparam>
+	/// <returns></returns>
+	public void Send<T>() where T:IDisposable,new()
+	{
+		var x = TempEventWrapper<T>.Get(this);
+		x.Dispose();	
+	}
+
+
+	/// <summary>
+	/// Creates a disposable dispatch wrapper. 
+	/// </summary>
+	/// <typeparam name="T">Type of the message payload</typeparam>
+	/// <returns></returns>
+	public TempEventWrapper<T> SendScope<T>() where T:IDisposable,new()
+	{
+		return TempEventWrapper<T>.Get(this);
+	}
+
+	public void SendRaw<T>(T message)
+    {
+        using (var tempHash = TempHashSet<EventDispatcher>.Get())
+        {
+            SendInternal(message, tempHash.hashSet);
+        }
+    }
+
+    private void SendInternal(object message, HashSet<EventDispatcher> sentToSet)
+    {
+        if (!sentToSet.Add(this) || message==null)
+        {
+            return;
+        }
+
+        // call all cached dispatchers (copy before execute in case any register listeners)
+        using (var callList = TempList<IBase>.Get())
+        {
+            callList.list.AddRange(GetOrCreateCache(message.GetType()));
+            foreach (var table in callList)
+            {
+                table.Dispatch(message);
+            }
+        }
+
+        m_otherDispatchers.RemoveWhere(x => x == null);
+        foreach (var mOtherDispatcher in m_otherDispatchers)
+        {
+            mOtherDispatcher.SendInternal(message, sentToSet);
+        }
+
+        if (this != Global)
+        {
+            Global.SendInternal(message,sentToSet);
+        }
+    }
+
+    private HashSet<IBase> GetOrCreateCache(Type messageType)
+	{
+		HashSet<IBase> dispatcherLookup;
+		if (!m_cacheDispatchForType.TryGetValue(messageType, out dispatcherLookup))
+		{
+			dispatcherLookup = m_cacheDispatchForType[messageType] = new HashSet<IBase>();
+
+			using (var list = TempList<KeyValuePair<Type, IBase>>.Get())
+			{
+				list.list.AddRange(m_dispatchers);
+				foreach (var element in list)
+				{
+					if (!CheckAssignableFrom(element.Key, messageType))
+					{
+						continue;
+					}
+
+					// cache dispatcher
+					dispatcherLookup.Add(element.Value);
+				}
+			}
+		}
+
+		return dispatcherLookup;
+	}
+
+	#region internal
+	interface IBase
+	{
+		void Dispatch(object message);
+	}
+
+	class Table<T> : IBase
+	{
+		public Action<T> dispatch;
+
+		public void Dispatch(object message)
+		{
+		    if (message is IConditionalEvent<T> condition && !condition.EventConditionMet)
+		    {
+		        return;
+		    }
+
+			if(dispatch!=null)
+			{
+				dispatch((T)message);
+			}	
+		}
+	}
+
+	private static Dictionary<Type, Dictionary<Type, bool>> s_typeAssignableFromCache = new Dictionary<Type, Dictionary<Type, bool>>();
+	private static bool CheckAssignableFrom(Type typeA, Type typeB)
+	{
+		Dictionary<Type, bool> typeSet;
+		if(!s_typeAssignableFromCache.TryGetValue(typeA, out typeSet))
+		{
+			s_typeAssignableFromCache[typeA] = typeSet = new Dictionary<Type, bool>();
+		}
+
+		bool result = false;
+		if(!typeSet.TryGetValue(typeB, out result))
+		{
+			result = typeSet[typeB] = typeA.IsAssignableFrom(typeB);
+		}
+
+		return result;
+	}
+
+    private HashSet<EventDispatcher>            m_otherDispatchers = new HashSet<EventDispatcher>();
+	private Dictionary<Type, IBase>             m_dispatchers = new Dictionary<Type, IBase>();
+	private Dictionary<Type, HashSet<IBase>>    m_cacheDispatchForType = new Dictionary<Type, HashSet<IBase>>();
+
+	Table<T> Get<T>()
+	{
+		IBase table;
+		if(!m_dispatchers.TryGetValue(typeof(T), out table))
+		{
+			table = m_dispatchers[typeof(T)] = new Table<T>();
+
+			// add this new table to any existing caches
+			foreach (var element in m_cacheDispatchForType)
+			{
+				if (!CheckAssignableFrom(typeof(T), element.Key))
+				{
+					continue;
+				}
+                
+				element.Value.Add(table);
+			}
+		}
+
+		return (Table<T>)table; 
+	}
+	#endregion
+
+	public class TempEventWrapper<T> : IDisposable where T:IDisposable,new()
+	{
+		private static readonly Queue<TempEventWrapper<T>> s_lists = new Queue<TempEventWrapper<T>>();
+		public T value = new T();
+		private EventDispatcher dispatcher;
+
+		// acquire a temporary instance
+		public static TempEventWrapper<T> Get(EventDispatcher dispatcher)
+		{
+			var result = s_lists.Count > 0
+				? s_lists.Dequeue()
+				: new TempEventWrapper<T>();
+
+			result.dispatcher = dispatcher;
+			return result;
+		}
+
+		public static implicit operator T(TempEventWrapper<T> from)
+		{
+			return from != null ? from.value : default(T);
+		}
+
+		// send then return to pool
+		public void Dispose()
+		{
+			if (dispatcher != null)
+			{
+				dispatcher.SendRaw(value);
+			}
+
+			value.Dispose();					
+			s_lists.Enqueue(this);
+		}
+	}
+}
+
+public interface IConditionalEvent<TSelfType> 
+{
+    bool EventConditionMet { get; }
+}
---
+++ Tests/EventDispatcherTest.cs
@@ -0,0 +1,448 @@
+﻿#if UNITY_5_6_OR_NEWER && (UNITY_EDITOR||(ENABLE_PLAYMODE_TESTS_RUNNER && PLAYMODE_TESTS) )
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.TestTools.Constraints;
+using Is = NUnit.Framework.Is;
+
+namespace Plugins.Cratesmith.Dispatcher.Editor {
+
+public class EventDispatcherTest
+{
+	[Test]
+	public void PrimitiveTypes()
+    {
+        bool boolLogged = false;
+        System.Action<bool> boolFunc = delegate(bool f)
+        {
+            boolLogged ^= true;
+            Debug.LogFormat("bool: {0}",f);
+        };
+
+        bool intLogged = false;
+        System.Action<int> intFunc = delegate(int f)
+        {
+            intLogged ^= true;
+            Debug.LogFormat("int: {0}",f);
+        };
+
+        bool floatLogged = false;
+        System.Action<float> floatFunc = delegate(float f)
+        {
+            floatLogged ^= true;
+            Debug.LogFormat("float: {0}",f);
+        };
+
+        var dispatcher = new EventDispatcher();
+        dispatcher.Add(boolFunc);
+        dispatcher.Add(intFunc);
+        dispatcher.Add(floatFunc);
+        dispatcher.SendRaw(1);
+        dispatcher.SendRaw(1.0f);
+        dispatcher.SendRaw(true);
+
+        dispatcher.Remove(boolFunc);
+        dispatcher.Remove(intFunc);
+        dispatcher.Remove(floatFunc);
+        dispatcher.SendRaw(1);
+        dispatcher.SendRaw(1.0f);
+        dispatcher.SendRaw(true);
+
+        Assert.IsTrue(boolLogged, "boolFunc wasn't triggered");
+        Assert.IsTrue(intLogged, "intFunc wasn't triggered");
+        Assert.IsTrue(floatLogged, "floatFunc wasn't triggered");
+	}
+
+    internal interface IInterfaceA : IDisposable        {}
+    internal interface IInterfaceB : IDisposable		{}
+    internal class BaseA : IInterfaceA                  {public void Dispose(){}}
+    internal class SubclassA : BaseA                    {}
+    internal class BaseB : IDisposable					{public void Dispose(){}}
+    internal class SubclassB : BaseB, IInterfaceB      {}
+    internal struct StructA : IInterfaceA               {public void Dispose(){}}
+    internal struct StructC : IDisposable				{public void Dispose(){}}
+
+
+    [Test]
+	public void PooledInterfacesClassesAndStructs()
+    {
+        var typeCounts = new Dictionary<System.Type, int>();
+        var dispatcher = new EventDispatcher();
+            
+        System.Func<System.Type, int> getTypeCount = (funcType) =>
+        {
+            if(!typeCounts.ContainsKey(funcType))
+            {
+                return 0;
+            }
+            return typeCounts[funcType];
+        };
+
+        System.Action<System.Type, System.Type> updateTypeCount = delegate(System.Type funcType, System.Type varType)
+        {
+			Profiler.BeginSample("UpdateTypeCount");
+            if(!typeCounts.ContainsKey(funcType))
+            {
+                typeCounts[funcType] = 1;
+            }
+            else
+            {
+                ++typeCounts[funcType];
+            }
+            //Debug.LogFormat("{0}Func: {1}", funcType.Name, varType.Name);
+			Profiler.EndSample();
+        };
+
+        System.Action<BaseA> baseAFunc = delegate(BaseA f) {updateTypeCount(typeof(BaseA), f.GetType());};
+        dispatcher.Add(baseAFunc);
+
+        System.Action<BaseB> baseBFunc = delegate(BaseB f) {updateTypeCount(typeof(BaseB), f.GetType());};
+        dispatcher.Add(baseBFunc);
+
+        System.Action<SubclassA> subAFunc = delegate(SubclassA f) {updateTypeCount(typeof(SubclassA), f.GetType());};
+        dispatcher.Add(subAFunc);
+
+        System.Action<SubclassB> subBFunc = delegate(SubclassB f) {updateTypeCount(typeof(SubclassB), f.GetType());};
+        dispatcher.Add(subBFunc);
+
+        System.Action<StructA> structAFunc = delegate(StructA f) {updateTypeCount(typeof(StructA), f.GetType());};
+        dispatcher.Add(structAFunc);
+
+        System.Action<StructC> structCFunc = delegate(StructC f) {updateTypeCount(typeof(StructC), f.GetType());};
+        dispatcher.Add(structCFunc);
+
+        System.Action<IInterfaceA> intAFunc = delegate(IInterfaceA f) {updateTypeCount(typeof(IInterfaceA), f.GetType());};
+        dispatcher.Add(intAFunc);
+
+        System.Action<IInterfaceB> intBFunc = delegate(IInterfaceB f) {updateTypeCount(typeof(IInterfaceB), f.GetType());};
+        dispatcher.Add(intBFunc);
+
+		// pre-send each type so they are pooled
+	    using (dispatcher.SendScope<BaseA>()) {}
+	    using (dispatcher.SendScope<SubclassA>()) {}
+	    using (dispatcher.SendScope<BaseB>()) {}
+	    using (dispatcher.SendScope<SubclassB>()) {}
+	        
+		typeCounts.Clear();
+		Assert.That(() =>
+		{
+			using (dispatcher.SendScope<BaseA>())
+			{
+			}
+		}, Is.Not.AllocatingGCMemory());
+
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+		
+	    Assert.That(() =>
+		{
+			typeCounts.Clear();
+			using (dispatcher.SendScope<SubclassA>()) {}			
+	    }, Is.Not.AllocatingGCMemory());
+	    Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+	    Assert.AreEqual(getTypeCount(typeof(BaseA)), 1);
+	    Assert.AreEqual(getTypeCount(typeof(SubclassA)), 1);
+	    Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+		
+	    Assert.That(() =>
+		{
+			typeCounts.Clear();
+			using (dispatcher.SendScope<BaseB>()) {}			
+	    }, Is.Not.AllocatingGCMemory());
+	    Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(BaseB)), 1);
+	    Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+	    Assert.That(() =>
+		{
+			typeCounts.Clear();
+			using (dispatcher.SendScope<SubclassB>()) {}			
+	    }, Is.Not.AllocatingGCMemory());
+	    Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+	    Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 1);
+	    Assert.AreEqual(getTypeCount(typeof(BaseB)), 1);
+	    Assert.AreEqual(getTypeCount(typeof(SubclassB)), 1);
+	    Assert.AreEqual(getTypeCount(typeof(StructC)), 0);		
+    }
+
+	[Test]
+	public void AddAndRemove()
+	{
+		var typeCounts = new Dictionary<System.Type, int>();
+		var dispatcher = new EventDispatcher();
+            
+		System.Func<System.Type, int> getTypeCount = (funcType) =>
+		{
+			if(!typeCounts.ContainsKey(funcType))
+			{
+				return 0;
+			}
+			return typeCounts[funcType];
+		};
+
+		System.Action<System.Type, System.Type> updateTypeCount = delegate(System.Type funcType, System.Type varType)
+		{
+			if(!typeCounts.ContainsKey(funcType))
+			{
+				typeCounts[funcType] = 1;
+			}
+			else
+			{
+				++typeCounts[funcType];
+			}
+			Debug.LogFormat("{0}Func: {1}", funcType.Name, varType.Name);
+		};
+
+		System.Action<BaseA> baseAFunc = delegate(BaseA f) {updateTypeCount(typeof(BaseA), f.GetType());};
+		dispatcher.SendRaw(new BaseA());
+		dispatcher.Add(baseAFunc);
+		dispatcher.Remove(baseAFunc);
+		dispatcher.Add(baseAFunc);
+
+		System.Action<BaseB> baseBFunc = delegate(BaseB f) {updateTypeCount(typeof(BaseB), f.GetType());};
+		dispatcher.Add(baseBFunc);
+		dispatcher.Remove(baseBFunc);
+		dispatcher.Add(baseBFunc);
+
+
+		System.Action<SubclassA> subAFunc = delegate(SubclassA f) {updateTypeCount(typeof(SubclassA), f.GetType());};
+		dispatcher.SendRaw(new SubclassA());
+		dispatcher.Add(subAFunc);
+		dispatcher.Remove(subAFunc);
+		dispatcher.Add(subAFunc);
+
+		System.Action<SubclassB> subBFunc = delegate(SubclassB f) {updateTypeCount(typeof(SubclassB), f.GetType());};
+		dispatcher.Add(subBFunc);
+		dispatcher.Remove(subBFunc);
+		dispatcher.Add(subBFunc);
+
+		System.Action<StructA> structAFunc = delegate(StructA f) {updateTypeCount(typeof(StructA), f.GetType());};
+		dispatcher.SendRaw(new StructA());
+		dispatcher.Add(structAFunc);
+		dispatcher.Remove(structAFunc);
+		dispatcher.Add(structAFunc);
+
+		System.Action<StructC> structCFunc = delegate(StructC f) {updateTypeCount(typeof(StructC), f.GetType());};
+		dispatcher.Add(structCFunc);
+		dispatcher.Remove(structCFunc);
+
+		System.Action<IInterfaceA> intAFunc = delegate(IInterfaceA f) {updateTypeCount(typeof(IInterfaceA), f.GetType());};
+		dispatcher.SendRaw(new StructA()); 
+		dispatcher.Add(intAFunc);
+		dispatcher.Remove(intAFunc);
+		dispatcher.Add(intAFunc);
+
+		System.Action<IInterfaceB> intBFunc = delegate(IInterfaceB f) {updateTypeCount(typeof(IInterfaceB), f.GetType());};
+		dispatcher.Add(intBFunc);
+		dispatcher.Remove(intBFunc);
+		dispatcher.Add(intBFunc);
+            
+		typeCounts.Clear();
+		dispatcher.SendRaw(new BaseA());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new SubclassA());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new StructA());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new BaseB());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new SubclassB());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new StructC());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+	}
+
+	[Test]
+	public void InterfacesStructsAndClasses()
+	{
+		var typeCounts = new Dictionary<System.Type, int>();
+		var dispatcher = new EventDispatcher();
+            
+		System.Func<System.Type, int> getTypeCount = (funcType) =>
+		{
+			if(!typeCounts.ContainsKey(funcType))
+			{
+				return 0;
+			}
+			return typeCounts[funcType];
+		};
+
+		System.Action<System.Type, System.Type> updateTypeCount = delegate(System.Type funcType, System.Type varType)
+		{
+			if(!typeCounts.ContainsKey(funcType))
+			{
+				typeCounts[funcType] = 1;
+			}
+			else
+			{
+				++typeCounts[funcType];
+			}
+			Debug.LogFormat("{0}Func: {1}", funcType.Name, varType.Name);
+		};
+
+		System.Action<BaseA> baseAFunc = delegate(BaseA f) {updateTypeCount(typeof(BaseA), f.GetType());};
+		dispatcher.Add(baseAFunc);
+
+		System.Action<BaseB> baseBFunc = delegate(BaseB f) {updateTypeCount(typeof(BaseB), f.GetType());};
+		dispatcher.Add(baseBFunc);
+
+		System.Action<SubclassA> subAFunc = delegate(SubclassA f) {updateTypeCount(typeof(SubclassA), f.GetType());};
+		dispatcher.Add(subAFunc);
+
+		System.Action<SubclassB> subBFunc = delegate(SubclassB f) {updateTypeCount(typeof(SubclassB), f.GetType());};
+		dispatcher.Add(subBFunc);
+
+		System.Action<StructA> structAFunc = delegate(StructA f) {updateTypeCount(typeof(StructA), f.GetType());};
+		dispatcher.Add(structAFunc);
+
+		System.Action<StructC> structCFunc = delegate(StructC f) {updateTypeCount(typeof(StructC), f.GetType());};
+		dispatcher.Add(structCFunc);
+
+		System.Action<IInterfaceA> intAFunc = delegate(IInterfaceA f) {updateTypeCount(typeof(IInterfaceA), f.GetType());};
+		dispatcher.Add(intAFunc);
+
+		System.Action<IInterfaceB> intBFunc = delegate(IInterfaceB f) {updateTypeCount(typeof(IInterfaceB), f.GetType());};
+		dispatcher.Add(intBFunc);
+            
+		typeCounts.Clear();
+		dispatcher.SendRaw(new BaseA());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new SubclassA());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new StructA());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 1);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new BaseB());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new SubclassB());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 1);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 0);
+
+		typeCounts.Clear();
+		dispatcher.SendRaw(new StructC());   
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructA)), 0);
+		Assert.AreEqual(getTypeCount(typeof(IInterfaceB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(BaseB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(SubclassB)), 0);
+		Assert.AreEqual(getTypeCount(typeof(StructC)), 1);
+	}
+}
+
+}
+#endif
